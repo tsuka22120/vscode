e4d416f679118ef966caf6c9167212dd079aee1f
ashin12345678 <22120@g.nagano-nct.ac.jp>
2024-06-21 09:31:53 +0900

    test

--- sort_ito/allsort.c
+++ sort_ito/allsort.c
@@ -7,45 +7,60 @@
 #include <string.h>
 #include <time.h>
 
-#define C 523
-#define index_num 12
+#define index_num 50
+#define print_on
 #define SWAP(a, b) ((a != b) && (a += b, b = a - b, a -= b))
 
-bool CheckSort(long ary[]);
+bool CheckSort(int64_t ary[]);
 
-void bogosort(long ary[]);
+void bogosort(int64_t ary[]);
 
-void selectionsort(long ary[]);
+void selectionsort(int64_t ary[]);
 
-void heapsort(long ary[]);
+void heapsort(int64_t ary[]);
 
-void bubblesort(long ary[]);
+void bubblesort(int64_t ary[]);
 
-void quicksort(long ary[]);
+void quicksort(int64_t ary[]);
 
-void MakeRandomAry(long ary[]);
+void MakeRandomAry(int64_t ary[]);
 
-void shuffle(long ary[]);
+int choicePivot(int64_t ary[], int64_t left, int64_t right, int* comparableNum);
 
+/// @brief leftとrightの真ん中を返す関数
+/// @param left 配列の左端の添え字
+/// @param right 配列の右端の添え字
+/// @return 配列の真ん中の添え字
+int choiceCenter(int64_t left, int64_t right);
+
+void shuffle(int64_t ary[]);
+
+/// @brief パーティション分割する関数
+/// @param ary 配列全体
+/// @param left 配列の分割する部分の左端
+/// @param right 配列の分割する部分の右端
+/// @param comparableNum 試行回数のポインタ
+void partitioning(int64_t ary[], int64_t left, int64_t right,
+                  int64_t* comparableNum);
 // 引数はスタートした時間
 void showTime(clock_t start, clock_t end);
 
-void showAry(long ary[], int size);
+void showAry(int64_t ary[], int64_t size);
 
 int main(void) {
-    long num[index_num];
+    int64_t num[index_num];
     srand((unsigned int)time(NULL));
     MakeRandomAry(num);
-    bogosort(num);
-    selectionsort(num);
-    heapsort(num);
-    bubblesort(num);
+    // bogosort(num);
+    // selectionsort(num);
+    // heapsort(num);
+    // bubblesort(num);
     quicksort(num);
     return 0;
 }
 
-bool CheckSort(long ary[]) {
-    int i = 0;
+bool CheckSort(int64_t ary[]) {
+    int64_t i = 0;
     for (i = 0; i < index_num - 1; i++) {
         if (ary[i] >= ary[i + 1]) {
             return false;
@@ -54,12 +69,12 @@ bool CheckSort(long ary[]) {
     return true;
 }
 
-void bogosort(long ary[]) {
+void bogosort(int64_t ary[]) {
     printf("ボゴソート開始\n");
-    long comparableNum = 0;
+    int64_t comparableNum = 0;
     clock_t start, end;
-    long num[index_num];
-    memcpy(num, ary, sizeof(long) * (index_num));
+    int64_t num[index_num];
+    memcpy(num, ary, sizeof(int64_t) * (index_num));
     printf("ソート前");
     showAry(num, index_num);
     start = clock();
@@ -78,14 +93,14 @@ void bogosort(long ary[]) {
     printf("ボゴソート終了\n");
 }
 
-void selectionsort(long ary[]) {
+void selectionsort(int64_t ary[]) {
     printf("選択ソート開始\n");
-    long comparableNum = 0;
+    int64_t comparableNum = 0;
     clock_t start, end;
-    int minimumNum = 0;
-    int i, j;
-    long num[index_num];
-    memcpy(num, ary, sizeof(long) * (index_num));
+    int64_t minimumNum = 0;
+    int64_t i, j;
+    int64_t num[index_num];
+    memcpy(num, ary, sizeof(int64_t) * (index_num));
     printf("ソート前");
     showAry(num, index_num);
     start = clock();
@@ -112,17 +127,17 @@ void selectionsort(long ary[]) {
     }
 }
 
-void heapsort(long ary[]) {
+void heapsort(int64_t ary[]) {
     printf("ヒープソート開始\n");
-    long comparableNum = 0;
+    int64_t comparableNum = 0;
     clock_t start, end;
-    int i;
-    long heap[index_num];
-    int heap_size = 1;
-    int index, childIndex;
-    long ans[index_num];
-    long num[index_num];
-    memcpy(num, ary, sizeof(long) * (index_num));
+    int64_t i;
+    int64_t heap[index_num];
+    int64_t heap_size = 1;
+    int64_t index, childIndex;
+    int64_t ans[index_num];
+    int64_t num[index_num];
+    memcpy(num, ary, sizeof(int64_t) * (index_num));
     printf("ソート前");
     showAry(num, index_num);
     start = clock();
@@ -182,13 +197,13 @@ void heapsort(long ary[]) {
     }
 }
 
-void bubblesort(long ary[]) {
+void bubblesort(int64_t ary[]) {
     printf("バブルソート開始\n");
-    int i, j;
-    long comparableNum = 0;
+    int64_t i, j;
+    int64_t comparableNum = 0;
     clock_t start, end;
-    long num[index_num];
-    memcpy(num, ary, sizeof(long) * (index_num));
+    int64_t num[index_num];
+    memcpy(num, ary, sizeof(int64_t) * (index_num));
     printf("ソート前");
     showAry(num, index_num);
     start = clock();
@@ -214,23 +229,114 @@ void bubblesort(long ary[]) {
     }
 }
 
-void quicksort(long ary[]){
+void quicksort(int64_t ary[]) {
     printf("クイックソート開始\n");
-    long left,right;
-    
+    int64_t left, right;
+    int64_t comparableNum = 0;
+    clock_t start, end;
+    int64_t num[index_num];
+    memcpy(num, ary, sizeof(int64_t) * (index_num));
+    printf("ソート前");
+    showAry(num, index_num);
+    start = clock();
+    left = 0;
+    right = index_num - 1;
+    partitioning(num, left, right, &comparableNum);
+    end = clock();
+    if (CheckSort(num) == false) {
+        printf("error\n");
+        printf("\n");
+    } else {
+        printf("ソート後");
+        showAry(num, index_num);
+        printf("試行回数:%ld\n", comparableNum);
+        showTime(start, end);
+        printf("クイックソート終了\n");
+    }
 }
 
-void MakeRandomAry(long ary[]) {
-    int i;
+void partitioning(int64_t ary[], int64_t left, int64_t right,
+                  int64_t* comparableNum) {
+    if (left >= right) {
+        return;
+    }
+    int64_t pivot;
+    int64_t leftIndex, rightIndex;
+    leftIndex = left;
+    rightIndex = right;
+    pivot = choicePivot(ary, left, right, comparableNum);
+    while (1) {
+        *comparableNum++;
+        while (ary[left] < pivot) {
+            left++;
+            *comparableNum++;
+        }
+        *comparableNum++;
+        while (ary[right] > pivot) {
+            right--;
+            *comparableNum++;
+        }
+        if (left >= right) {
+            break;
+        }
+        SWAP(ary[right], ary[left]);
+        left++;
+        right--;
+    }
+    printf("%ld\n", *comparableNum);
+    partitioning(ary, leftIndex, left - 1, comparableNum);
+    partitioning(ary, right + 1, rightIndex, comparableNum);
+}
+
+int choicePivot(int64_t ary[], int64_t left, int64_t right,
+                int* comparableNum) {
+    int64_t center;
+    center = (right - left) / 2 + left;
+    *comparableNum++;
+    if (ary[right] > ary[center]) {
+        *comparableNum++;
+        if (ary[center] > ary[left]) {
+            return ary[center];
+        } else {
+            return ary[left];
+        }
+    }
+    *comparableNum++;
+    if (ary[center] > ary[left]) {
+        *comparableNum++;
+        if (ary[left] > ary[right]) {
+            return ary[left];
+        }
+        *comparableNum++;
+        if (ary[left] < ary[right]) {
+            return ary[right];
+        }
+    }
+    *comparableNum++;
+    if (ary[left] > ary[right]) {
+        *comparableNum++;
+        if (ary[right] > ary[center]) {
+            return ary[right];
+        }
+        *comparableNum++;
+        if (ary[right] < ary[center]) {
+            return ary[center];
+        }
+    }
+    return ary[left];
+}
+
+void MakeRandomAry(int64_t ary[]) {
+    int64_t i;
     for (i = 0; i < index_num; i++) {
         ary[i] = i + 1;
     }
     shuffle(ary);
 }
 
-void shuffle(long ary[]) {
-    int i;
-    int j;
+void shuffle(int64_t ary[]) {
+    int64_t i;
+    int64_t j;
     for (i = 0; i < index_num; i++) {
         j = rand() % index_num;
         SWAP(ary[i], ary[j]);
@@ -256,11 +362,13 @@ void showTime(clock_t start, clock_t end) {
     }
 }
 
-void showAry(long ary[], int size) {
-    int i;
+void showAry(int64_t ary[], int64_t size) {
+#ifdef print_on
+    int64_t i;
     printf("[ ");
     for (i = 0; i < size; i++) {
         printf("%ld ", ary[i]);
     }
     printf("]\n");
+#endif
 }
