b3e0cab55de5b4b6d6a743e473c472b8f0f8a1c7
ashin12345678 <22120@g.nagano-nct.ac.jp>
2024-07-04 18:23:24 +0900

    変更

--- algo_ito/hash.c
+++ algo_ito/hash.c
@@ -6,30 +6,40 @@
 /// @brief hash化する関数
 /// @param key ハッシュする値
 /// @return ハッシュ化された値
-int hash(int key) { return key % MAX; }
+int hash(int key) { return key * key % MAX; }
 
 /// @brief 文字をASCIIコードに変換する関数
 /// @param c 文字
 /// @return ASCIIコード
-int ascii(char c) { return (int)c; }
+int ascii(char c[]) { return (int)c[0]; }
 
 int main() {
     FILE *from, *to;
-    from = fopen("dist.all.last.txt", "r");
-    to = fopen("hash.txt", "w");
-    char c;
+    int count[100] = {0};
+    from = fopen("algo_ito/dist.all.last.txt", "r");
+    to = fopen("algo_ito/hash.csv", "w");
+    if(from == NULL){
+        printf("read file not open!\n");
+        return -1;
+    }
+    if(to == NULL){
+        printf("write file not open!\n");
+        return -1;
+    }
     int num, sum = 0;
     while (1) {
-        c = fgetc(from);
-        printf("%c\n",c);
-        if (c == EOF) break;
-        if('\n' == c){
-            fprintf(to,"%d\n",hash(sum));
-        }
-        num = ascii(c);
-        if (num >= 65 && num <= 90) {
+        if ((num = fgetc(from)) == EOF) break;
+        if('\n' == num){
+            fprintf(to, "%d,\n", hash(sum));
+            sum = 0;
+        }else if (num >= 65 && num <= 90) {
             sum += num;
         }
     }
+    // for (int i = 0; i < MAX; i++) {
+    //     fprintf(to, "%d,\n", count[i]);
+    // }
+    fclose(from);
+    fclose(to);
     return 0;
 }
