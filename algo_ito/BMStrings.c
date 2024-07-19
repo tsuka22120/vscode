c02d6f1436cbe0ac6cb1c6f27dbdd749a7dcbc2e
ashin12345678 <22120@g.nagano-nct.ac.jp>
2024-07-19 09:13:49 +0900

    koer

--- algo_ito/BMStrings.c
+++ algo_ito/BMStrings.c
@@ -5,11 +5,14 @@
 #define MAX 100
 #define SKIPTABLENUM 0x7e - 0x20
 
-void makeSkipTable(char strings[], char searchWord[]);
+void makeSkipTable(int skipTable[], char searchWord[], int searchWord_len);
 
 int main(void) {
     char strings[MAX + 1], searchWord[MAX + 1];
-    int str1_len, str2_len, i, j, count = 0;
+    int skipTable[SKIPTABLENUM];
+    int strings_len, searchWord_len, i, j, count = 0;
+    strings_len = strlen(strings);
+    searchWord_len = strlen(searchWord);
     printf("被検索文字列を入力してください(%d字以内しか読み込まれません)\n",
            MAX);
     scanf("%100s", strings);
@@ -22,24 +25,36 @@ int main(void) {
     }
     printf("被検索文字列: [ %s ]\n", strings);
     printf("検索文字列: [ %s ]\n", searchWord);
-    makeSkipTable(strings, searchWord);
+    makeSkipTable(skipTable, searchWord, searchWord_len);
+    for (i = 0; i < strings_len - searchWord_len + 1; i++) {
+        for (j = searchWord_len - 1; j >= 0; j--) {
+            if (strings[i + j] == searchWord[j]) {
+                if (j == 0) {
+                    printf("検索文字列が見つかりました(%d文字目)\n", i);
+                    break;
+                }
+            } else {
+                i += skipTable[(int)strings[i + j] - 0x20];
+            }
+        }
+    }
+    return 0;
 }
 
-void makeSkipTable(char strings[], char searchWord[]) {
-    int skipTable[SKIPTABLENUM];
+void makeSkipTable(int skipTable[], char searchWord[], int searchWord_len) {
     int i, j;
     for (i = 0; i < SKIPTABLENUM; i++) {
-        for (j = 0; j < strlen(searchWord); j++) {
+        for (j = searchWord_len - 2; j >= 0; j--) {
             if ((char)(0x20 + i) == searchWord[j]) {
-                skipTable[i] = strlen(searchWord) - j;
+                skipTable[i] = searchWord_len - j - 1;
                 break;
             } else {
-                skipTable[i] = strlen(searchWord);
+                skipTable[i] = searchWord_len;
             }
         }
     }
     for (i = 1; i < SKIPTABLENUM + 1; i++) {
-        printf("%c:%d ", 0x20 + i - 1, skipTable[i - 1]);
+        printf("%c:%2d ", 0x20 + i - 1, skipTable[i - 1]);
         if (0 == i % 9) {
             printf("\n");
         }
